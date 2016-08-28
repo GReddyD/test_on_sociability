16ccf496770a1db247e057a11a6e1c239af9d3ee
greddyd <greddy553@yandex.ru>
2016-08-28 08:50:19 -0700

    init version

---
+++ tets_on_sociability.rb
@@ -0,0 +1,12 @@
+require_relative "testq.rb"
+
+prog_name = "\"Ваш уровень общительности\"" #Название программы
+puts "Добрый день! Ответьте пожалуйста честно на несколько вопросов, чтобы узнать #{prog_name}\n\r"
+
+testq = TestQ.new
+
+while testq.next_question == true do
+
+end
+
+testq.results_test(testq.gets_balls)
